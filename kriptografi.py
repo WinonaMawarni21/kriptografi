1b627d48ae6bb99942ee2a724752651c09859659
WinonaMawarni21 <101933463+WinonaMawarni21@users.noreply.github.com>
2022-03-20 16:53:47 +0700

    Update kriptografi.py

--- kriptografi.py
+++ kriptografi.py
@@ -1,54 +1,48 @@
-#rumus enkripsi = (n - key) % 26
-#rumus dekripsi = (n + key) % 26
-#n = merupakan urutan dari abjad yang diinput 
-#key = merupakan kunci dekripsi atau enkripsi
-#26 =merupakan jumlah dari seluruh abjad
+
 
 abjad = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] 
-#abjad berfungsi untuk menampung nilai abjad yang ada
 
 #fungsi enkripsi dengan parameter abjad
-def enkripsi(abjad): # membuat fungsi yang bertujuan untuk menerapkan caesar cipher
-    kalimat = input("Kalimat : ") #input kalimat yang akan di enkripsi
-    key = int(input("Key : ")) #kunci untuk pergeseran abjad (enkripsi)
+def enkripsi(abjad): 
+    kalimat = input("Kalimat : ") 
+    key = int(input("Key : ")) 
 
-    kalimat = kalimat.lower() #kalimat di konversi ke huruf kecil semua
-    hasil = '' #membuat variable string untuk memuat hasil enkripsi
+    kalimat = kalimat.lower() 
+    hasil = '' 
 
-    for char in kalimat: #membuat perulangan untuk mengenkripsi kalimat
-      if char in abjad: #abjad kalimat dipecah 1 1 dan di cek apakah terdapat dalam value abjad
-        n = abjad.index(char) #jika ada maka nilai index dariabjad yang ditemukan disimpan dalam variable n
-        encrypt = (n - key) % 26 #rumus enkripsi
-        convert = abjad[encrypt] #konversi nilai string ke hasil enkripsi
-        hasil = hasil + convert #membuat variable baru yang memuat hasil enkripsi
+    for char in kalimat: 
+      if char in abjad: 
+        n = abjad.index(char) 
+        encrypt = (n - key) % 26 
+        convert = abjad[encrypt]
+        hasil = hasil + convert 
       else:
-          hasil = hasil + ' ' #jika karakter yang dimasukkan bukan berasal dari abjad A - Z maka akan menjadi spasi
+          hasil = hasil + ' ' 
 
-    print(f"Hasil : {hasil}") #hasil dari perulangan untuk enkripsi kalimat di tampilkan 
+    print(f"Hasil : {hasil}") 
 
 #fungsi dekripsi dengan parameter abjad
 def dekripsi(abjad):
-    kalimat = input("Kalimat Enkripsi : ")  #input kalimat yang akan di dekripsi
-    key = int(input("Key : ")) #kunci untuk pergeseran abjad (dekripsi)
+    kalimat = input("Kalimat Enkripsi : ")  
+    key = int(input("Key : ")) 
 
-    kalimat = kalimat.upper() #kalimat di konversi ke huruf besar semua
-    hasil = '' #membuat variable string untuk memuat hasil enkripsi
-
-    for char in kalimat: #membuat perulangan untuk mengenkripsi kalimat
-        if char in abjad: #abjad kalimat dipecah 1 1 dan di cek apakah terdapat dalam value abjad
-          n = abjad.index(char) #jika ada maka nilai index dariabjad yang ditemukan disimpan dalam variable n
-          encrypt = (n + key) % 26 #rumus dekripsi
-          convert = abjad[encrypt] #konversi nilai string ke hasil dekripsi
-          hasil = hasil + convert #membuat variable baru yang memuat hasil enkripsi
+    kalimat = kalimat.upper() 
+    hasil = '' 
+    for char in kalimat: 
+        if char in abjad: 
+          n = abjad.index(char) 
+          encrypt = (n + key) % 26 
+          convert = abjad[encrypt] 
+          hasil = hasil + convert
         else:
-            hasil = hasil + ' '  #jika karakter yang dimasukkan bukan berasal dari abjad A - Z maka akan menjadi spasi
+            hasil = hasil + ' '  
 
-    print(f"Hasil : {hasil}") #hasil dari perulangan untuk enkripsi kalimat di tampilkan 
+    print(f"Hasil : {hasil}") 
 
 #menampilkan menu
 pilihan = 'y' 
 
-while (pilihan == 'y'): # membuat pilihan untuk melakukan enkripsi atau dekripsi
+while (pilihan == 'y'): 
   print("Menu : ")
   print("1. Enkripsi Data")
   print("2. Dekripsi Data")
@@ -56,12 +50,12 @@ while (pilihan == 'y'): # membuat pilihan untuk melakukan enkripsi atau dekripsi
 
   menu = input("Pilihan Menu : ")
 
-  if menu == '1': # memasukkan kalimat yang akan di enkripsi
-    enkripsi(abjad) # memanggil fungsi Caesar_cipher untuk mengenkripsi kalimat
-  elif menu == '2': # memasukkan kalimat yang akan di enkripsi
-    dekripsi(abjad) # memanggil fungsi Caesar_cipher untuk mendekripsi kalimat
+  if menu == '1': 
+    enkripsi(abjad) 
+  elif menu == '2': 
+    dekripsi(abjad) 
   elif menu == '3':
     print("progam selesai, terima kasih")
     break 
   else:
-    print("Menu tidak ditemukan") #jika memilih menu yang tidak ada di pilihan maka akan ditampilkan "menu tidak ditemukan" 
+    print("Menu tidak ditemukan") 
